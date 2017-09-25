24f0bae1879585020cbefe38a9e36bc0598e4480
Miguel Garcia <miguel@ADRIANAs-Mac-Pro.local>
2017-09-25 16:43:44 -0500

    Initial Commit

---
+++ K-Means-Segmentacion-Mejorado/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  K-Means-Segmentacion-Mejorado
+//
+//  Created by Miguel Garcia on 9/25/17.
+//  Copyright © 2017 Miguel Garcia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
